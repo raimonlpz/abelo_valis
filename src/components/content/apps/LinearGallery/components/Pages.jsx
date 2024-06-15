f15998658ed6a64c9da900f5415fae9756677515
Raimon López <raimon@bokokode.com>
2024-06-15 23:12:40 +0200

    fixed order

--- src/components/content/apps/LinearGallery/components/Pages.jsx
+++ src/components/content/apps/LinearGallery/components/Pages.jsx
@@ -13,15 +13,16 @@ export default function Pages() {
         floatIntensity={.15} // Up/down float intensity, defaults to 1
     >
 
-        <Page position={[width * 5, 0, 0]} urls={['abelo/img19.webp', 'abelo/img20.webp', 'abelo/img21.webp']}  />
-        <Page position={[width * 7, 0, 0]} urls={['abelo/img1.webp', 'abelo/img2.webp', 'abelo/img3.webp']} />
+        <Page position={[width * 5, 0, 0]} urls={['abelo/img10.webp', 'abelo/img20.webp', 'abelo/img3.webp']}  />
+        <Page position={[width * 7, 0, 0]} urls={['abelo/img1.webp', 'abelo/img22.webp', 'abelo/img21.webp']} />
         <Page position={[width * 8, 0, 0]} urls={['abelo/img4.webp', 'abelo/img17.webp', 'abelo/img18.webp']} />
+        <Page position={[width * 6, 0, 0]} urls={['abelo/img11.webp', 'abelo/img7.webp', 'abelo/img24.webp']} />
         <Page position={[width * 4, 0, 0]} urls={['abelo/img16.webp', 'abelo/img17.webp', 'abelo/img18.webp']} />
-        <Page position={[width * 2, 0, 0]} urls={['abelo/img10.webp', 'abelo/img11.webp', 'abelo/img12.webp']} />
+        <Page position={[width * 2, 0, 0]} urls={['abelo/img23.webp', 'abelo/img22.webp', 'abelo/img12.webp']} />
         <Page position={[-width * 1, 0, 0]} urls={['abelo/img1.webp', 'abelo/img2.webp', 'abelo/img3.webp']} />
-        <Page position={[width * 0, 0, 0]} urls={['abelo/img22.webp', 'abelo/img23.webp', 'abelo/img24.webp']} />
-        <Page position={[width * 1, 0, 0]} urls={['abelo/img7.webp', 'abelo/img8.webp', 'abelo/img9.webp']} />
-        <Page position={[width * 6, 0, 0]} urls={['abelo/img22.webp', 'abelo/img23.webp', 'abelo/img24.webp']} />
+        <Page position={[width * 0, 0, 0]} urls={['abelo/img19.webp', 'abelo/img2.webp', 'abelo/img24.webp']} />
+        <Page position={[width * 1, 0, 0]} urls={['abelo/img23.webp', 'abelo/img8.webp', 'abelo/img9.webp']} />
+      
         <Page position={[width * 3, 0, 0]} urls={['abelo/img13.webp', 'abelo/img14.webp', 'abelo/img15.webp']} />
 
       </Float>

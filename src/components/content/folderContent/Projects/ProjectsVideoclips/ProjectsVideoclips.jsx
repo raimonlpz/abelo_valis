08e35543463a743c16b0f6fee8b6fd7ba4e36089
Raimon López <raimon@bokokode.com>
2024-07-15 19:15:18 +0200

    finished Videoclips vieww

--- src/components/content/folderContent/Projects/ProjectsVideoclips/ProjectsVideoclips.jsx
+++ src/components/content/folderContent/Projects/ProjectsVideoclips/ProjectsVideoclips.jsx
@@ -1,4 +1,4 @@
-import React, { forwardRef, useState } from 'react'
+import React, { forwardRef, useEffect, useState } from 'react'
 import styles from './ProjectsVideoclips.module.css'
 import Draggable from 'react-draggable'
 
@@ -33,7 +33,11 @@ const ProjectsVideoclips = forwardRef(({
                 locations: "Plaza de Azca, Madrid, Madrid, Spain (Square)",
                 company: 'Roma'
             },
-            imgs: ['/projects/videoclips/alizz1.png', '/projects/videoclips/alizz2.png', '/projects/videoclips/alizz3.png']
+            imgs: ['/projects/videoclips/alizz1.png', '/projects/videoclips/alizz2.png', '/projects/videoclips/alizz3.png'],
+            links: {
+                youtube: 'https://www.youtube.com/watch?v=WBjUZIVwDDA',
+                imdb: 'https://www.imdb.com/title/tt32252314'
+            }
         },
         {
             id: 2,
@@ -41,18 +45,86 @@ const ProjectsVideoclips = forwardRef(({
             subtitles: ['Music Video', '2020', '3m'],
             info: {
                 director: 'Nysu',
-                stars: ['Abelo Valis', 'Jennifer Bucovineanu'],
+                stars: ['Abelo Valis, Jennifer Bucovineanu'],
                 storyline: "Music video for New Order's song 'Be a Rebel'.",
                 release: 'December 2, 2020 (UK)',
                 language: 'English',
                 locations: "United Kingdom",
                 company: 'Garage Films'
             },
-            imgs: ['/projects/videoclips/no2.png', '/projects/videoclips/no1.png', '/projects/videoclips/no3.png']
+            imgs: ['/projects/videoclips/no2.png', '/projects/videoclips/no1.png', '/projects/videoclips/no3.png'],
+            links: {
+                youtube: 'https://www.youtube.com/watch?v=JOoyPT6RoF4',
+                imdb: 'https://www.imdb.com/title/tt22789114/'
+            }
+        },
+        {
+            id: 3,
+            title: 'Fuel Fandango ft. María José Llergo: Mi huella',
+            subtitles: ['Music Video', '2021', '4m'],
+            info: {
+                director: 'Alex Gargot',
+                stars: ['Abelo Valis, Fuel Fandango, María José Llergo'],
+                storyline: "Music video for Fuel Fandango's song 'Mi huella'.",
+                release: 'March 25, 2021 (Spain)',
+                language: 'Spanish',
+                locations: "Spain",
+                company: '-'
+            },
+            imgs: ['/projects/videoclips/fuel1.png', '/projects/videoclips/fuel2.png', '/projects/videoclips/fuel3.png'],
+            links: {
+                youtube: 'https://www.youtube.com/watch?v=JOoyPT6RoF4',
+                imdb: 'https://www.imdb.com/title/tt27367421'
+            }
+        },
+        {
+            id: 4,
+            title: 'Juancho Marqués feat. Daniela Garsal: Te Acuerdas Que',
+            subtitles: ['Music Video', '2020', '3m'],
+            info: {
+                director: 'José Pérez',
+                stars: ['Daniela Garsal, Juancho Marqués, Laura Moray'],
+                storyline: 'Music video for "Te Acuerdas Que" by Juancho Marqués ft. Daniela Garsal',
+                release: 'August 14, 2020 (Spain)',
+                language: 'Spanish',
+                locations: "Spain",
+                company: 'Instinto Films'
+            },
+            imgs: ['/projects/videoclips/juancho1.png', '/projects/videoclips/juancho2.png', '/projects/videoclips/juancho3.png'],
+            links: {
+                youtube: 'https://www.youtube.com/watch?v=eNRoiM5NHYQ',
+                imdb: 'https://www.imdb.com/title/tt14336192'
+            }
         }
     ]
     
-    const [project, setProject] = useState(data[0])
+    const [indexProject, setIndexProject] = useState(0)
+    const [project, setProject] = useState(data[indexProject])
+    const [mainImg, setMainImg] = useState(data[indexProject].imgs[0])
+
+    const changeProjectPlus = () => {
+        if (indexProject < data.length - 1) {
+            setIndexProject(indexProject + 1)
+            setProject(data[indexProject + 1])
+        } else {
+            setIndexProject(0)
+            setProject(data[0])
+        }
+    }
+
+    const changeProjectMinus = () => {
+        if (indexProject > 0) {
+            setIndexProject(indexProject - 1)
+            setProject(data[indexProject - 1])
+        } else {
+            setIndexProject(data.length - 1)
+            setProject(data[data.length - 1])
+        }
+    }
+
+    useEffect(() => {
+        setMainImg(data[indexProject].imgs[0])
+    }, [indexProject])
 
     return (
         <Draggable
@@ -174,8 +246,8 @@ const ProjectsVideoclips = forwardRef(({
                 <div className={styles.right_inner_wrapper}>
                     <div className={styles.right_top_bar}>
                     <div className={styles.right_top_bar_left}>
-                        <img src="/images/icons/left_arrow.png" alt="left_arrow" />
-                        <img src="/images/icons/right_arrow.png" alt="right_arrow" />
+                        <img src="/images/icons/left_arrow.png" alt="left_arrow" onClick={changeProjectMinus} />
+                        <img src="/images/icons/right_arrow.png" alt="right_arrow" onClick={changeProjectPlus} />
                         <p className={styles.left_text}>Videoclips</p>
                     </div>
                     <div className={styles.right_top_bar_right}>
@@ -192,17 +264,21 @@ const ProjectsVideoclips = forwardRef(({
                     <div className={styles.right_inner_content}>
                         <div className={styles.finder_panel}>
                             <div className={styles.finder_panel_left}>
+                                <div className={styles.panel_links}>
+                                    <div onClick={() => window.open(project.links.youtube)}>Youtube</div>
+                                    <div onClick={() => window.open(project.links.imdb)}>IMDB</div>
+                                </div>
                                 <div className={styles.panel_main_photo}>
-                                    <img src={project.imgs[0]} alt="" />
+                                    <img src={mainImg} alt="" />
                                 </div>
                                 <div className={styles.panel_gallery}>
                                     <div>
-                                        Vídeos & Fotos
+                                        Más Fotos
                                     </div>
                                     <div>
-                                        <img src={project.imgs[0]} alt="" />
-                                        <img src={project.imgs[1]} alt="" />
-                                        <img src={project.imgs[2]} alt="" />
+                                        <img src={project.imgs[0]} alt="" onClick={() => setMainImg(project.imgs[0])} />
+                                        <img src={project.imgs[1]} alt="" onClick={() => setMainImg(project.imgs[1])} />
+                                        <img src={project.imgs[2]} alt="" onClick={() => setMainImg(project.imgs[2])} />
                                     </div>
                                 </div>
                             </div>

fa8c785b3d140acaa5048a9bc3b5c39223f953c7
Guillem231 <pedret.guillem@gmail.com>
2024-07-28 20:55:11 +0200

    responsive folder downloads + handleontouchstat

--- src/components/content/folderContent/Downloads/FolderDownloads.jsx
+++ src/components/content/folderContent/Downloads/FolderDownloads.jsx
@@ -2,30 +2,34 @@ import React, { useState, forwardRef } from 'react'
 import styles from './FolderDownloads.module.css'
 import Draggable from 'react-draggable';
 
-const FolderDownloads = React.forwardRef(({ 
-  isFolderDownloadsOpen, 
-  closeFolderDownloads, 
-  maximizeApp, 
-  minimizeApp, 
-  onStartDrag, 
+const FolderDownloads = React.forwardRef(({
+  isFolderDownloadsOpen,
+  closeFolderDownloads,
+  maximizeApp,
+  minimizeApp,
+  onStartDrag,
   onStopDrag,
   //
   openFolderProjects,
-  openFolderDocuments, 
+  openFolderDocuments,
   openCircGallery,
   openMakarradas,
-  openInterviews, 
-  openFormations, 
-  openAboutMe 
+  openInterviews,
+  openFormations,
+  openAboutMe
 }, ref) => {
 
-  const [folderOnfocus,setFolderOnfocus] = useState(null)
+  const [folderOnfocus, setFolderOnfocus] = useState(null)
+  const handleTouchStart = (callback) => (e) => {
+    e.preventDefault();
+    callback();
+  };
 
-    return (
-      <>
+  return (
+    <>
       <Draggable onStart={onStartDrag} onStop={onStopDrag}>
         <div
-          ref={ref}   
+          ref={ref}
           className={styles.wrapper}
           style={{
             visibility: isFolderDownloadsOpen ? "visible" : "hidden",
@@ -43,25 +47,25 @@ const FolderDownloads = React.forwardRef(({
               <img
                 onTouchStart={minimizeApp}
                 onClick={minimizeApp}
-                className={styles.left_corner_button_img}
+                className={styles.left_corner_button_minimize}
                 src="/images/icons/minimise.png"
                 alt="icns"
               />
               <img
                 onTouchStart={maximizeApp}
                 onClick={maximizeApp}
-                className={styles.left_corner_button_img}
+                className={styles.left_corner_button_maximize}
                 src="/images/icons/zoom.png"
                 alt="icns"
               />
-               <div className={styles.closeX}>&#10005;</div>
-               <div className={styles.miniX}>-</div>
+              <div className={styles.closeX}>&#10005;</div>
+              <div className={styles.miniX}>-</div>
             </div>
             <div className={styles.left_menu_wrapper}>
               <h5 className={styles.menu_title}>Favoritos</h5>
               <ul className={styles.left_ul}>
-                
-              <li className={styles.left_li} onClick={openFolderProjects}>
+
+                <li className={styles.left_li} onClick={openFolderProjects} onTouchStart={handleTouchStart(openFolderProjects)}>
                   <img
                     className={styles.blue_icon_img}
                     src="/images/blueicons/star.png"
@@ -69,7 +73,7 @@ const FolderDownloads = React.forwardRef(({
                   />
                   <p className={styles.left_li_text}>Proyectos</p>
                 </li>
-                <li className={styles.left_li} onClick={() => window.open('/files/AbeloValis_CV.pdf', '_blank')}>
+                <li className={styles.left_li} onClick={() => window.open('/files/AbeloValis_CV.pdf', '_blank')} onTouchStart={handleTouchStart(() => window.open('/files/AbeloValis_CV.pdf', '_blank'))}>
                   <img
                     className={styles.blue_icon_img}
                     src="/images/blueicons/file.png"
@@ -77,7 +81,7 @@ const FolderDownloads = React.forwardRef(({
                   />
                   <p className={styles.left_li_text}>CV_2024</p>
                 </li>
-                <li className={styles.left_li} onClick={openAboutMe}>
+                <li className={styles.left_li} onClick={openAboutMe} onTouchStart={handleTouchStart(openAboutMe)}>
                   <img
                     className={styles.blue_icon_img}
                     src="/images/blueicons/clock.png"
@@ -85,7 +89,7 @@ const FolderDownloads = React.forwardRef(({
                   />
                   <p className={styles.left_li_text}>Sobre Mi</p>
                 </li>
-                <li className={styles.left_li} onClick={openFormations}>
+                <li className={styles.left_li} onClick={openFormations} onTouchStart={handleTouchStart(openFormations)}>
                   <img
                     className={styles.blue_icon_img}
                     src="/images/blueicons/file.png"
@@ -93,7 +97,7 @@ const FolderDownloads = React.forwardRef(({
                   />
                   <p className={styles.left_li_text}>Formación</p>
                 </li>
-                <li className={styles.left_li} onClick={openInterviews}>
+                <li className={styles.left_li} onClick={openInterviews} onTouchStart={handleTouchStart(openInterviews)}>
                   <img
                     className={styles.blue_icon_img}
                     src="/images/blueicons/files.png"
@@ -101,7 +105,7 @@ const FolderDownloads = React.forwardRef(({
                   />
                   <p className={styles.left_li_text}>Prensa</p>
                 </li>
-                <li className={styles.left_li} onClick={openCircGallery}>
+                <li className={styles.left_li} onClick={openCircGallery} onTouchStart={handleTouchStart(openCircGallery)}>
                   <img
                     className={styles.blue_icon_img}
                     src="/images/blueicons/folder.png"
@@ -109,7 +113,7 @@ const FolderDownloads = React.forwardRef(({
                   />
                   <p className={styles.left_li_text}>Fotos</p>
                 </li>
-                <li className={styles.left_li} onClick={openMakarradas}>
+                <li className={styles.left_li} onClick={openMakarradas} onTouchStart={handleTouchStart(openMakarradas)}>
                   <img
                     className={styles.blue_icon_img}
                     src="/images/blueicons/download.png"
@@ -120,7 +124,7 @@ const FolderDownloads = React.forwardRef(({
               </ul>
               <h5 className={styles.menu_title}>iCloud</h5>
               <ul className={styles.left_ul}>
-                <li className={styles.left_li} onClick={openFolderDocuments}>
+                <li className={styles.left_li} onClick={openFolderDocuments} onTouchStart={handleTouchStart(openFolderDocuments)}>
                   <img
                     className={styles.blue_icon_img}
                     src="/images/blueicons/cloud.png"
@@ -131,7 +135,7 @@ const FolderDownloads = React.forwardRef(({
               </ul>
               {/* <h5 className={styles.menu_title}>Tags</h5>
               <ul className={styles.left_ul}> */}
-                {/* <li className={styles.left_li}>
+              {/* <li className={styles.left_li}>
                   <img
                     className={styles.blue_icon_img}
                     src="/images/blueicons/orangeTag.png"
@@ -155,7 +159,7 @@ const FolderDownloads = React.forwardRef(({
                   />
                   <p className={styles.left_li_text}>Green Tag</p>
                 </li> */}
-                {/* <li className={styles.left_li}>
+              {/* <li className={styles.left_li}>
                   <img
                     className={styles.blue_icon_img}
                     src="/images/blueicons/allTags.png"
@@ -186,40 +190,39 @@ const FolderDownloads = React.forwardRef(({
             </div>
             <div className={styles.right_inner_content}>
               <div className={styles.row}>
-                <div className={styles.folderWrapper} onClick={() => setFolderOnfocus('libros')} onMouseLeave={() => setFolderOnfocus(null)} onDoubleClick={() => window.open('https://archive.org/', '_blank')}>
-                  <img 
-                    src="/images/icons/archiveorg.png" 
-                    alt="folder" 
-                    style={{'width': '80px'}} 
-                    className={`${folderOnfocus === 'libros' ? styles.selectedIcon : ''}`} 
+                <div className={styles.folderWrapper} onClick={() => setFolderOnfocus('libros')} onMouseLeave={() => setFolderOnfocus(null)} onDoubleClick={() => window.open('https://archive.org/', '_blank')} onTouchStart={handleTouchStart(() => window.open('https://archive.org/', '_blank'))}>
+                  <img
+                    src="/images/icons/archiveorg.png"
+                    alt="folder"
+                    style={{ 'width': '80px' }}
+                    className={`${folderOnfocus === 'libros' ? styles.selectedIcon : ''}`}
                   />
                   <span className={`${folderOnfocus === 'libros' ? styles.selected : ''}`}>Libros</span>
                 </div>
-                <div className={styles.folderWrapper} onClick={() => setFolderOnfocus('emule')} onMouseLeave={() => setFolderOnfocus(null)} onDoubleClick={() => window.open('https://audiotools.in/', '_blank')}>
-                  <img 
-                    src="/images/icons/emule.png" 
-                    alt="folder" 
-                    style={{'width': '80px'}} 
-                    className={`${folderOnfocus === 'emule' ? styles.selectedIcon : ''}`} 
+                <div className={styles.folderWrapper} onClick={() => setFolderOnfocus('emule')} onMouseLeave={() => setFolderOnfocus(null)} onDoubleClick={() => window.open('https://audiotools.in/', '_blank')} onTouchStart={handleTouchStart(() => window.open('https://audiotools.in/', '_blank'))}>
+                  <img
+                    src="/images/icons/emule.png"
+                    alt="folder"
+                    style={{ 'width': '80px' }}
+                    className={`${folderOnfocus === 'emule' ? styles.selectedIcon : ''}`}
                   />
                   <span className={`${folderOnfocus === 'emule' ? styles.selected : ''}`}>Música/Plugins</span>
                 </div>
-                <div className={styles.folderWrapper} onClick={() => setFolderOnfocus('utorrent')} onMouseLeave={() => setFolderOnfocus(null)} onDoubleClick={() => window.open('https://www.utorrent.com/', '_blank')}>
-                  <img 
-                    src="/images/icons/utorrent.png" 
-                    alt="folder" 
-                    style={{'width': '80px'}} 
-                    className={`${folderOnfocus === 'utorrent' ? styles.selectedIcon : ''}`} 
+                <div className={styles.folderWrapper} onClick={() => setFolderOnfocus('utorrent')} onMouseLeave={() => setFolderOnfocus(null)} onDoubleClick={() => window.open('https://www.utorrent.com/', '_blank')} onTouchStart={handleTouchStart(() => window.open('https://www.utorrent.com/', '_blank'))}>
+                  <img
+                    src="/images/icons/utorrent.png"
+                    alt="folder"
+                    style={{ 'width': '80px' }}
+                    className={`${folderOnfocus === 'utorrent' ? styles.selectedIcon : ''}`}
                   />
                   <span className={`${folderOnfocus === 'utorrent' ? styles.selected : ''}`}>xxx</span>
                 </div>
-           
-                <div className={styles.folderWrapper} onClick={() => setFolderOnfocus('pirate')} onMouseLeave={() => setFolderOnfocus(null)} onDoubleClick={() => window.open('https://thepiratebay3.co/', '_blank')}>
-                  <img 
-                    src="/images/icons/piratebay.png" 
-                    alt="folder" 
-                    style={{'width': '80px'}} 
-                    className={`${folderOnfocus === 'pirate' ? styles.selectedIcon : ''}`} 
+                <div className={styles.folderWrapper} onClick={() => setFolderOnfocus('pirate')} onMouseLeave={() => setFolderOnfocus(null)} onDoubleClick={() => window.open('https://thepiratebay3.co/', '_blank')} onTouchStart={handleTouchStart(() => window.open('https://thepiratebay3.co/', '_blank'))}>
+                  <img
+                    src="/images/icons/piratebay.png"
+                    alt="folder"
+                    style={{ 'width': '80px' }}
+                    className={`${folderOnfocus === 'pirate' ? styles.selectedIcon : ''}`}
                   />
                   <span className={`${folderOnfocus === 'pirate' ? styles.selected : ''}`}>🚬🥃💊</span>
                 </div>
@@ -241,12 +244,12 @@ const FolderDownloads = React.forwardRef(({
                 </div> */}
               </div>
               <div className={styles.row}>
-              <div className={styles.folderWrapper} onClick={() => setFolderOnfocus('stremio')} onMouseLeave={() => setFolderOnfocus(null)} onDoubleClick={() => window.open('https://www.stremio.com/', '_blank')}>
-                  <img 
-                    src="/images/icons/stremio.ico" 
-                    alt="folder" 
-                    style={{'width': '80px'}} 
-                    className={`${folderOnfocus === 'stremio' ? styles.selectedIcon : ''}`} 
+                <div className={styles.folderWrapper} onClick={() => setFolderOnfocus('stremio')} onMouseLeave={() => setFolderOnfocus(null)} onDoubleClick={() => window.open('https://www.stremio.com/', '_blank')}  onTouchStart={handleTouchStart(() => window.open('https://www.stremio.com/', '_blank'))}>
+                  <img
+                    src="/images/icons/stremio.ico"
+                    alt="folder"
+                    style={{ 'width': '80px' }}
+                    className={`${folderOnfocus === 'stremio' ? styles.selectedIcon : ''}`}
                   />
                   <span className={`${folderOnfocus === 'stremio' ? styles.selected : ''}`}>Pelis</span>
                 </div>
@@ -322,8 +325,8 @@ const FolderDownloads = React.forwardRef(({
         </div>
 
       </Draggable>
-      </>
-    );
-  });
+    </>
+  );
+});
 
 export default FolderDownloads

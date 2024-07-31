c0828f67159bdb726f525fc40fa551d9c4837166
Raimon López <raimon@bokokode.com>
2024-07-31 18:03:54 +0200

    loading fix

--- src/components/content/auth/Booth.jsx
+++ src/components/content/auth/Booth.jsx
@@ -21,7 +21,7 @@ export default function Boot({ restart, sleep, setBooting }) {
 
   useInterval(
     () => {
-      const newPercent = percent + (isMobile ? 0.6 : 0.35);
+      const newPercent = percent + (isMobile ? 1 : 0.35);
       if (newPercent >= 100) {
         setTimeout(() => {
           setBooting(false);
